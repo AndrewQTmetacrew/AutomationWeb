a9bcced92660770c2b8dc5856fe8480281698237
AndrewQT <andrewnguyen1898@gmail.com>
2024-10-05 10:16:32 +0700

    Update 10/05/2024

--- tests/test_Ex.py
+++ tests/test_Ex.py
@@ -1,9 +1,4 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from page_objects.exceptions_page import ExceptionPage
 
 
--- tests/test_negative.py
+++ tests/test_negative.py
@@ -1,7 +1,4 @@
-import time
-
 import pytest
-from selenium.webdriver.common.by import By
 from page_objects.login_page import LoginPage
 
 
